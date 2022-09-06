e6d3be93a8fdc41fc99288ebdda9af2f315ca4d8
Sathvik Mulukutla <darthkitten2228@gmail.com>
2022-09-07 00:11:04 +0530

    git commit -m "Added while loop to launcher"

--- launch.py
+++ launch.py
@@ -1,14 +1,17 @@
 import os
 
-
-print("Welcome to PyGames! Please choose a game to play!")
-game = input("Your options are:\nRock, Paper, Scissors\nHangman\nTic Tac Toe\nChoose Wisely. ")
-game_names = {'Rock, Paper, Scissors':'rps',
-              'Hangman':'hangman',
-              'Tic Tac Toe':'tic_tac_toe'}
-
 def imp(name):
-    module = __import__(name)
-    return module
+        module = __import__(name)
+        return module
+        
+while True:
 
-imp(game_names[game])
+    print("Welcome to PyGames! Please choose a game to play!")
+    game = input("Your options are:\nRock, Paper, Scissors\nHangman\nTic Tac Toe\nChoose Wisely. ")
+    game_names = {'Rock, Paper, Scissors':'rps',
+                'Hangman':'hangman',
+                'Tic Tac Toe':'tic_tac_toe'}
+
+    
+
+    imp(game_names[game])
