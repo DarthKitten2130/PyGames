4f777df719852feb27d4113bad9915aa97b94c7f
Sathvik Mulukutla <darthkitten2228@gmail.com>
2022-09-07 00:16:41 +0530

    git commit -m "Added quit function to rps"

--- launch.py
+++ launch.py
@@ -3,12 +3,12 @@ import os
 def imp(name):
         module = __import__(name)
         return module
-        
+
 while True:
 
     print("Welcome to PyGames! Please choose a game to play!")
     game = input("Your options are:\nRock, Paper, Scissors\nHangman\nTic Tac Toe\nChoose Wisely. ")
-    game_names = {'Rock, Paper, Scissors':'rps',
+    game_names = {'rps':'rps',
                 'Hangman':'hangman',
                 'Tic Tac Toe':'tic_tac_toe'}
 
