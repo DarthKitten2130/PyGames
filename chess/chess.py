6a49e2135d0d0d086fd5a1653803c80ab7d48951
Sathvik Mulukutla <darthkitten2228@gmail.com>
2022-09-07 20:39:55 +0530

    git commit -m "Started Chess"

--- chess/chess.py
+++ chess/chess.py
@@ -1,1 +1,15 @@
-import pygame
+import pygame as pg
+from os import chdir
+
+pg.init()
+
+screen = pg.display.set_mode((800,800))
+win = False
+bgimg = pg.image.load('Chess_Board.png')
+bgimg = pg.transform.scale(bgimg,(800,800))
+
+
+while win == False:
+    pg.event.get()
+    screen.blit(bgimg,(0,0))
+    pg.display.flip()
