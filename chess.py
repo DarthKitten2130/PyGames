bc7f327c427465280b1989100c7eec7a37919dc6
Sathvik Mulukutla <darthkitten2228@gmail.com>
2022-10-07 20:57:47 +0530

    git commit -m

--- chess.py
+++ chess.py
@@ -37,6 +37,11 @@ squares = ( 'a1','b1','c1','d1','e1','f1','g1','h1',
             'a7','b7','c7','d7','e7','f7','g7','h7',
             'a8','b8','c8','d8','e8','f8','g8','h8')
 
+start_squares = ( 'a1','b1','c1','d1','e1','f1','g1','h1',
+            'a2','b2','c2','d2','e2','f2','g2','h2',
+            'a7','b7','c7','d7','e7','f7','g7','h7',
+            'a8','b8','c8','d8','e8','f8','g8','h8')
+
 turn = 'white'
 
 while running == True:
@@ -51,7 +56,7 @@ while running == True:
         screen.fill((0, 104, 14))
         for square in squares:
             exec(square+'.Blit(screen)')
-            
+
         for unit in whites:
             exec(unit+'.Blit(screen)')
 
